6b82c339d702340bb5e0cf4b428c9aab15f8f602
Ganade <bganade@gmail.com>
2022-05-25 20:51:32 -0300

    Add Ex009
    Ex009 Done

---
+++ Curso em Video/Exercicios/Ex001 - Ex010/Ex009.py
@@ -0,0 +1,13 @@
+# Write a program that reads any integer and displays its multiplication table on the screen.
+
+n = int(input("Enter a integer number to display it's multiplication table: "))
+print(f"{n} x 1 = {n*1}")
+print(f"{n} x 2 = {n*2}")
+print(f"{n} x 3 = {n*3}")
+print(f"{n} x 4 = {n*4}")
+print(f"{n} x 5 = {n*5}")
+print(f"{n} x 6 = {n*6}")
+print(f"{n} x 7 = {n*7}")
+print(f"{n} x 8 = {n*8}")
+print(f"{n} x 9 = {n*9}")
+print(f"{n} x 10 = {n*10}")
