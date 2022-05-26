46079034d07621601618f956c3b0ebe6224dcd9b
Bruno Ganade Alves <bganade@gmail.com>
2022-05-26 12:11:52 -0300

    Ex007
    Ex007 done

--- Curso em Video/Exercicios/Ex007.py
+++ Curso em Video/Exercicios/Ex007.py
@@ -1,2 +1,6 @@
-# Desenvolva um programa que leia às duas notas de um aluno, calcule e mostre a sua média.
-#
+# Develop a program that reads a student's two grades, calculates and displays their average.
+
+grade_1 = float(input("Enter the first grade: "))   # Reads the first grade.
+grade_2 = float(input("Enter the second grade: "))  # Reads the second grade.
+average = (grade_1 + grade_2) / 2   # Calculates the average between the first and second grade
+print(f"the student school average is {average:.3}")    # show the student's average
