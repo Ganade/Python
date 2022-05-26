d3be371b8a77b96f6c2b840baca553bcde70e835
Ganade <bganade@gmail.com>
2022-05-25 21:05:07 -0300

    Ex010
    Ex010 Done

---
+++ Curso em Video/Exercicios/Ex001 - Ex010/Ex010.py
@@ -0,0 +1,5 @@
+# Create a program that reads how much money a person has in their wallet and shows how many dollars they can buy.
+
+wallet = float(input("How much money do you have in your wallet?: "))
+dolar = 4.77
+print(f"You can by {wallet/dolar:.4} dolars")
