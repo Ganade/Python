105ac8f02fcde95c06ba8661906206fd9d1928a9
Bruno Ganade Alves <bganade@gmail.com>
2022-05-28 18:58:07 -0300

    Ex014
    Ex014 Done

---
+++ Curso em Video/Exercicios/Ex011 - Ex020/Ex014.py
@@ -0,0 +1,5 @@
+# Write a program that converts a temperature by typing in degrees Celsius and converts it to degrees Fahrenheit.
+
+temperature = float(input("Enter a temperature in Celsius to be converted in Fahrenheit: "))
+Fahrenheit = temperature * 1.8 + 32
+print(f"You typed {temperature}ºC and that temperature in fahrenheit will be {Fahrenheit}ºF")
