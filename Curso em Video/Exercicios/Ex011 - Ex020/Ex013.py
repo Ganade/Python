b7a2326f06b59b5f050dc688657240a19ff602d9
Bruno Ganade Alves <bganade@gmail.com>
2022-05-28 19:06:34 -0300

    Ex015
    Ex015 done

--- Curso em Video/Exercicios/Ex011 - Ex020/Ex013.py
+++ Curso em Video/Exercicios/Ex011 - Ex020/Ex013.py
@@ -1,6 +1,6 @@
 # Make an algorithm that reads an employee's salary and displays their new salary, with a 15% increase.
 
-salary = float(input("Enter the employers salary to be readjusted: "))
-readjust = 15
-salary_readjust = salary * (1+(readjust/100))
+salary = float(input("Enter the employers salary to be readjusted: "))  # Salary of the employer
+readjust = 15   # How much will be to readjust
+salary_readjust = salary * (1+(readjust/100))   # Calculates to readjust
 print(f"the salary is {salary}, with the readjust will be {salary_readjust:.4}")
