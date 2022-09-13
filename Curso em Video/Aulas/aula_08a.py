5e2a98522a519291c356aaff4f4542ac9061e414
Bruno Ganade Alves <bganade@gmail.com>
2022-09-13 15:18:23 -0300

    Class 08 added

--- Curso em Video/Aulas/aula_08a.py
+++ Curso em Video/Aulas/aula_08a.py
@@ -1,9 +1,12 @@
 """
-Class 08A
+    Class 08A
 """
 
 from math import sqrt
-num = int(input("Enter a number: "))
+from random import randint
 
+num = int(input("Enter a number: "))
+rand = randint(1, 10)
 square = sqrt(num)
 print(f"the square root of {num} is {square}")
+print(f"Your random number is {rand}")
