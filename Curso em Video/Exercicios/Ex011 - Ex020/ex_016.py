28048a96afdb9cf3e5522d51597a99d4aac33531
Bruno Ganade Alves <bganade@gmail.com>
2022-09-13 17:18:30 -0300

    Ex 016
    Done Exercise 016

--- Curso em Video/Exercicios/Ex011 - Ex020/ex_016.py
+++ Curso em Video/Exercicios/Ex011 - Ex020/ex_016.py
@@ -1,3 +1,11 @@
 """
-Exercise 16 - 
+Exercise 16 - breaking a number
 """
+# make a program that reads any real number from the keyboard and displays its entire portion on the
+# screen.
+
+from math import floor
+
+numb = float(input("Enter a real number please: "))
+entire_number = floor(numb)
+print(f"you typed the number {numb} and its integer part is {entire_number}")
