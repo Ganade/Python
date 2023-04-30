b4b8e5bfda9a1a31ea96d417951d71dfec78abe2
Bruno Ganade Alves <bganade@gmail.com>
2023-04-30 10:12:19 -0300

    Improvement of the game

--- Alura/Aulas/divination.py
+++ Alura/Aulas/divination.py
@@ -2,14 +2,15 @@ print("*******************************")
 print("Welcome to the divination game.")
 print("*******************************")
 
-secret_number = 43 #teste
+secret_number = 43
 attempts = 5
-current_round = 1
 
-while current_round <= attempts:
-    print("Round {} of {}".format(current_round, attempts))
+for rounds in range(1, attempts + 1):
+    print("Round {} of {}".format(rounds, attempts))
     number_typed = int(input("Type a number between 1 to 100: "))
-
+    if number_typed < 1 or number_typed > 100:
+        print("You should type a number between 1 to 100")
+        continue
     win = number_typed == secret_number
     higher = number_typed > secret_number
 
@@ -18,9 +19,9 @@ while current_round <= attempts:
     if win:
         print("You got the number right.")
         print("***You Win***")
+        break
     else:
         if higher:
             print("You got the number wrong, you typed a higher number.")
         else:
             print("You got the number wrong, you typed a lower number.")
-    current_round = current_round + 1
